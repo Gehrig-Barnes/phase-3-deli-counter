201653f9bbf41dce8be616f43bf5c85c62ce3eff
Gehrig-Barnes <gehrigbarnes@gmail.com>
2022-01-31 12:08:55 -0600

    commit tree 345e18fee06127d2b606e27d842a990ef2d77df2

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+require "pry"
+
+def katz_deli 
+    ["harry", "marry", "barry", "clarry"]
+  end
+
+def line(katz_deli)
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    else
+        in_line = "The line is currently:"
+        katz_deli.map.with_index(1) do |person, index|
+            in_line << " #{index}. #{person}"
+            
+        end
+    puts in_line
+    end
+end
+
+def take_a_number(katz_deli, person)
+    katz_deli.push(person)
+    puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
+end
+
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli.first}."
+        katz_deli.shift
+    end
+end
+
+binding.pry
